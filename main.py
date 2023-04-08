602cb4c413898424e7f69d4c961e97b9352423d9
yeabsiraM290 <yeabsiraM@outlook.com>
2023-04-08 12:00:51 +0300

    remove info

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ OPTIONS.add_argument("window-size=1366,768")
 
 
 bot = FacebookBot(
-    email="se.nahom.tamru@gmail.com", password="123field45", options=OPTIONS
+    email="", password="", options=OPTIONS
 )
 s = sched.scheduler(time.time, time.sleep)
 
